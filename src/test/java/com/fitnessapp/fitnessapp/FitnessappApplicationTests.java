2de4bb925456e247a25c7f7cc18fecaaffa06b00
Mayur Khadgi <mayurkhadgi6@gmail.com>
2025-01-26 00:13:04 +0530

    intial commit

---
+++ src/test/java/com/fitnessapp/fitnessapp/FitnessappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessapp.fitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
