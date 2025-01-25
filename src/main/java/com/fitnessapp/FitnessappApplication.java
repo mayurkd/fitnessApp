1ba49b46d4af63fbf794ecd160534e5d9330a0bb
Mayur Khadgi <mayurkhadgi6@gmail.com>
2025-01-26 00:16:35 +0530

    naming issue resolved

--- src/main/java/com/fitnessapp/FitnessappApplication.java
+++ src/main/java/com/fitnessapp/FitnessappApplication.java
@@ -1,4 +1,4 @@
-package com.fitnessapp.fitnessapp;
+package com.fitnessapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
